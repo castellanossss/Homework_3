bb966449406cc16c840ca8538b60ce2c77c0ec1e
Brayan daniel fuquene sandoval <brayan.fuquene01@uptc.edu.co>
2023-04-09 14:20:39 +0200

    Created necessary class for patient creation

---
+++ src/view/PatientRegistrationView.java
@@ -0,0 +1,35 @@
+package view;
+
+import java.util.Scanner;
+
+public class PatientRegistrationView {
+    private Scanner scanner;
+
+    public PatientRegistrationView() {
+        scanner = new Scanner(System.in);
+    }
+
+    public void printBorderedTitleMessage() {
+        String message = "Patient Registration";
+        int length = message.length();
+        String border = "╔" + "═".repeat(length + 2) + "╗";
+        System.out.println(border);
+        System.out.println("║ " + message + " ║");
+        border = "╚" + "═".repeat(length + 2) + "╝";
+        System.out.println(border);
+    }
+
+    public String showMessageData(String message) {
+        System.out.print(message);
+        return scanner.next();
+    }
+
+    public void printBorderedMessage(String message) {
+        int messageLength = message.length();
+        String border = "+" + "-".repeat(messageLength + 2) + "+";
+        System.out.println(border);
+        System.out.println("| " + message + " |");
+        System.out.println(border);
+    }
+
+}
